545d6d91758d470334c1dfd163fa284935285504
Qwerci <theowerci@gmail.com>
2023-06-26 10:58:00 +0000

    added logic to get all food items

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-
+	"log"
+	"strconv"
 	"github.com/Qwerci/restaurant/database"
 	"github.com/Qwerci/restaurant/models"
 	"github.com/gin-gonic/gin"
@@ -20,9 +21,50 @@ var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
+
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 20
+		}
+
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1
+		}
+
+		startIndex :=  (page-1) * recordPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+		groupStage := bson.D{
+			{
+				"$group", bson.D{
+					{"_id", "null"}, 
+					{"total_count", bson.D{{"$sum", 1}}},
+					{"data", bson.D{{"$push", "$$ROOT"}}}}}}
+
+		projectStage := bson.D{
+			{
+				"$project", bson.D{
+					{"_id", 0},
+					{"total_count", 1},
+					{"food_items", bson.D{
+						{"$slice", []interface{}{"$data", startIndex, recordPerPage}}}},
+				}}}
+
+		result, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage})
+		defer cancel()
+		var allFoods []bson.M
+		if err = result.All(ctx, &allFoods); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allFoods[0])
 
 	}
 }
+
 
 
 func GetFood() gin.HandlerFunc {
