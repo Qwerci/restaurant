2c6e13cbec179025fd4688fde6aa4cb98a60adc9
Qwerci <theowerci@gmail.com>
2023-07-04 23:55:26 +0000

    added logic to update an ordered items by id

--- controllers/orderitemController.go
+++ controllers/orderitemController.go
@@ -85,6 +85,48 @@ func CreateOrderItem() gin.HandlerFunc {
 
 func UpdateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context){
+		var ctx,cancel = context.WithTimeout(context.Background(), 100 * time.Second)
+		var orderItem models.OrderItem
+		orderItemId := c.Param("order_item_id")
+		filter := bson.M{"order_item_id": orderItemId}
 
+		var updateObj primitive.D
+
+		if orderItem.Unit_price != nil {
+			updateObj = append(updateObj, bson.E{Key:"unit_price", Value: *&orderItem.Unit_price})
+		}
+
+		if orderItem.Quantity != nil {
+			updateObj = append(updateObj, bson.E{Key: "quantity", Value: *orderItem.Quantity})
+		}
+
+		if orderItem.Food_id != nil {
+			updateObj = append(updateObj, bson.E{Key:"food_id", Value: *orderItem.Food_id})
+		}
+
+		orderItem.Updated_at = time.Now()
+		updateObj = append(updateObj, bson.E{Key:"updated_at", Value: orderItem.Updated_at})
+
+		upsert := true
+		opt := options.Update().SetUpsert(upsert)
+
+		result, err := orderitemCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{Key: "$set", Value: updateObj},
+			},
+			opt,
+		)
+
+		if err != nil {
+			msg := "Order item update failed"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		defer cancel()
+
+		c.JSON(http.StatusOK, result)
 	}
 }
